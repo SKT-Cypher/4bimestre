ba3fb8c93931f05aca6d8d875bba5f0ef4939516
Vidal <guilhermevidalpekka@gmail.com>
2022-10-11 07:15:03 -0300

    prova pronta feita sozinho Guilherme Vidal Info A

--- src/signos/index.js
+++ src/signos/index.js
@@ -6,10 +6,13 @@ export default function MesDia(){
     const [resultado, setResultado] = useState('')
 
   function logica(){
-    let msg =''
-    if (mes != 'outubro' && mes != 'setembro')
-    return msg = ' mes invalido'
-    setResultado(msg)
+   if((mes === "setembro" && dia >= 23 && dia <=30 || (mes === "outubro" && dia >= 22 && dia <=31 )))
+  {
+    return setResultado("É libra")
+  }
+  else{
+    return setResultado ("Não é libra")
+  }
 
    
   }
@@ -20,11 +23,11 @@ export default function MesDia(){
         <h1>Signos</h1>
         <div> mes <input type="text" value={mes} onChange={e => setMes(e.target.value)}></input> </div>
         <div> dia <input type="number" value={dia} onChange={e => setDia(Number(e.target.value))}></input> </div>
-        <div>  {resultado} </div>
         <button onClick={logica}>Vereficar</button>
+        <div>  {resultado} </div>
 
 
-  
+      
         
     
 

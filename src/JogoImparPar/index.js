a0442f36060003446ce3bf0f1b73a74219787329
Vidal <guilhermevidalpekka@gmail.com>
2022-10-25 20:38:46 -0300

    par ou impar pronto e media mediaAluno quase

--- src/JogoImparPar/index.js
+++ src/JogoImparPar/index.js
@@ -1,10 +1,69 @@
 import './index.scss';
-import { useState } from 'react';
+
 
 export default function JogoImparPar(){
 
+  var EscolhaDaPessoa= '';
+  var EscolhaDoComputador = ''
 
-    return (
-        <h1>Jogo do Impar ou Par</h1>
+  var EscolhaDaPessoa = prompt("par ou impar ?");
+var DedosDaPessoa = prompt("Digite quantos dedos você coloca:");
+if(EscolhaDaPessoa === "par")
+{
+EscolhaDoComputador = "impar";
+}
+else
+{
+EscolhaDoComputador = "par";
+}
+var GeraNumero = Math.random()*10;
+var DedosDoComputador = Math.round(GeraNumero);
+function calcula()
+{
+var SomaDosDedos = parseInt(DedosDaPessoa) + parseInt(DedosDoComputador);
+var IdentificaPar = SomaDosDedos % 2;
+if(IdentificaPar === 0)
+{
+if(EscolhaDaPessoa === "par")
+{
+return "Você pediu "+EscolhaDaPessoa+" e<br>VOCÊ VENCEU! <br>o computador pôs "+DedosDoComputador+" dedos.<br>e você pôs "+DedosDaPessoa+" dedos.<br>o total deu "+SomaDosDedos+" dedos, que é PAR!";
+}
+else
+{
+return "Você pediu "+EscolhaDaPessoa+" e<br>O COMPUTADOR VENCEU! <br>o computador pôs "+DedosDoComputador+" dedos<br>e você pôs "+DedosDaPessoa+" dedos. <br>o total deu "+SomaDosDedos+" dedos, que é PAR!";
+}
+}
+else
+{
+if(EscolhaDaPessoa === "impar")
+{
+return "Você pediu "+EscolhaDaPessoa+" e<br>VOCÊ VENCEU! <br>o computador pôs "+DedosDoComputador+" dedos.<br>e você pôs "+DedosDaPessoa+" dedos.<br>o total deu "+SomaDosDedos+" dedos, que é IMPAR!";
+}
+else
+{
+return "Você pediu "+EscolhaDaPessoa+" e<br>O COMPUTADOR VENCEU! <br>o computador pôs "+DedosDoComputador+" dedos<br>e você pôs "+DedosDaPessoa+" dedos. <br>o total deu "+SomaDosDedos+" dedos, que é IMPAR!";
+}
+}
+}
+var Resultado = calcula();
+document.write ("<p align='center'><font size='30' color='blue'>"+Resultado);
+
+
+  
+      
+
+    return(
+
+     
+      <main>
+      
+     
+      
+   
+     
+      </main>
+
+
+        
     )
 }
--- src/mediaAluno/index.js
+++ src/mediaAluno/index.js
@@ -1,23 +1,25 @@
 import './index.scss';
-
 import { useState } from 'react';
 
 export default function MediaAlunos(){
     const [aluninhos, setAluninhos] = useState(0);
-    const [nota, setNota] = useState(0);
+    const [nota, setNota] = useState([]);
     const [resultado, setResultado] = useState(0);
-
- 
+    let arr = [];
+    let input = <input  className="inputt" type='number' ></input> 
+   
     function contarAlunos(){
-        let arr = [];
         
+        let alun =  'aluno:'
+       
+       
         
        for(let cont = 1; cont <= aluninhos; cont++)
-       arr.push( cont + "\n")
-         setResultado (arr);
-    
-       
+       arr.push(alun, cont, input , "\n") 
+       setResultado(arr);
       }
+
+      
 
     return (
 
@@ -28,6 +30,11 @@ export default function MediaAlunos(){
       
         <button className="botao" onClick={contarAlunos}>Vereficar</button>
         <div className='resultado'>{resultado}</div>
+
+        <button className="botao" onClick={''}>Vereficar Nota alta</button>
+       
+        <button className="botao" onClick={''}>Vereficar Nota baixa</button>
+        <button className="botao" onClick={''}>Vereficar Media</button>
         </main>
     )
 }
